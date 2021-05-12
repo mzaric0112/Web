d7bd7d515a95a0aacc3e0fa5d008cd463c6a1ad0
mzaric1998 <zaric.in22.2018@uns.ac.rs>
2021-05-12 13:38:13 +0200

    Dodavanje html stranica za registraciju

--- Web/src/main/java/com/example/Web/Model/Korisnik.java
+++ Web/src/main/java/com/example/Web/Model/Korisnik.java
@@ -33,6 +33,19 @@ public class Korisnik implements Serializable{
     public Korisnik() {
     }
 
+    public Korisnik(Long id, String korisnickoIme, String ime, String prezime, String lozinka, String telefon, String email, Date datumRodjenja, boolean aktivan, Uloga uloga) {
+        this.id = id;
+        this.korisnickoIme = korisnickoIme;
+        this.ime = ime;
+        this.prezime = prezime;
+        this.lozinka = lozinka;
+        this.telefon = telefon;
+        this.email = email;
+        this.datumRodjenja = datumRodjenja;
+        this.aktivan = aktivan;
+        this.uloga = uloga;
+    }
+
     @Override
     public String toString() {
         return "Korisnik{" +
