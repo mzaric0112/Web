a0b925771ccd79cff192051f091c11e3dad016c7
mzaric1998 <zaric.in22.2018@uns.ac.rs>
2021-07-04 11:18:55 +0200

    Pregled trenera - admin

---
+++ Web/src/main/resources/static/js/brisanjeTrenera.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+      $.ajax({
+                      type: "GET",
+                      url: "http://localhost:8181/api/trener",
+                      dataType: "json",
+                      success: function (response) {                              // ova f-ja se izvršava posle uspešnog zahteva
+                          console.log("SUCCESS:\n", response);                    // ispisujemo u konzoli povratnu vrednost radi provere
+
+                          for (let trener of response) {                        // prolazimo kroz listu svih zaposlenih
+                              let row = "<tr>";                                   // kreiramo red za tabelu
+                              row += "<td>" + trener.ime + "</td>";       // ubacujemo podatke jednog zaposlenog u polja
+                              row += "<td>" + trener.prezime + "</td>";
+                              row += "<td>" + trener.korisnickoIme + "</td>";
+                              row += "<td>" + trener.datumRodjenja + "</td>";
+                              row += "</tr>";                                     // završavamo kreiranje reda
+
+                              $('#treneri').append(row);                        // ubacujemo kreirani red u tabelu čiji je id = employees
+                          }
+                      },
+                      error: function (response) {
+                      }
+
+      });
+});
